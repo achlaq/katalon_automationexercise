a519bc6803d2da0e54442c9e003b5a7d3743f3d2
Achlaq Al Arif <achlaqalarif@yahoo.co.id>
2024-04-05 10:25:18 +0700

    PUSH

--- Test Suites/Login - Logout.ts
+++ Test Suites/Login - Logout.ts
@@ -12,6 +12,20 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>77385e7f-bf2a-4a63-893a-7b499ade6388</testSuiteGuid>
    <testCaseLink>
+      <guid>be717025-e951-461a-b01a-7771b31d85d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register - With Existing Email</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ce6f1fa-8141-4e31-952e-687addb9ab5a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login - Incorrect</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
       <guid>e8a9f930-ef24-41ad-9fd7-62e77a6060ec</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -23,6 +37,20 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34301626-a728-4fef-9d33-8a125bfdf9f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add review on product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1cda2af8-dc8c-474f-9007-981b5046f2b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Subscription in home page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
